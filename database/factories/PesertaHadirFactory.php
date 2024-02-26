75fd35e1148c5a340c56854b04831f14428d2ef8
Aldhi Xar <aldhi.x@gmail.com>
2024-02-26 21:28:24 +0700

    seeder

---
+++ database/factories/PesertaHadirFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PesertaHadir>
+ */
+class PesertaHadirFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'berita_acara_id'=>1,
+            'nama' => fake()->name(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,21 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@localhost.com',
+        ]);
+
+        \App\Models\BeritaAcara::create([
+            'jadwal_id'=>1,
+            'ruang_id'=>1,
+            'pengawas1_id'=>5,
+            'pengawas2_id'=>6,
+            'kelas'=>'XII TGP A',
+            'catatan'=>'Tertib dan aman'
+        ]);
+
+        \App\Models\PesertaHadir::factory(10)->create();
+        \App\Models\PesertaAbsen::factory(8)->create();
     }
 }
